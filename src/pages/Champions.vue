6e715129603c1bf0976f6a7a1413b088367f117f
Raphael Avelar <rafael-avelar@hotmail.com>
2021-06-14 01:04:21 -0300

    Implementando scroll-area na listagem de champions

--- src/pages/Champions.vue
+++ src/pages/Champions.vue
@@ -1,27 +1,38 @@
 <template>
-  <div class="fit row items-stretch content-stretch">
-    <q-page class="fit" style="margin-left: 18rem; margin-right: 18rem;">
-      <div class="row justify-center">
-        <template v-for="(champion, i) in champions" class="">
-          <div :key="i" class="q-py-md" style="height: 6.25rem; width: 8.75rem">
-            <div class="row justify-center">
-              <q-img src="https://placeimg.com/500/300/nature" spinner-color="black" style="height: 48px; max-width: 48px" />
-            </div>
-            <span class="row justify-center">{{ champion.name }}</span>
-          </div>
-        </template>
+  <div class="fit row items-stretch content-stretch bg-dark">
+    <q-page class="fit shadow-24" style="margin-left: 18rem; margin-right: 18rem;">
+      <div class="row justify-center q-pa-md">
+        <q-input rounded outlined v-model="searchChampion" dense dark style="width: 35rem" placeholder="Digite o nome do Campeão...">
+          <template v-slot:append>
+            <q-icon name="search" dense style="color: white"/>
+          </template>
+        </q-input>
       </div>
+
+      <q-scroll-area style="height: calc(100vh - 122px)">
+        <div class="row justify-center">
+          <template v-for="(champion, i) in champions">
+            <div :key="i" class="q-py-md" style="height: 6.25rem; width: 8.75rem">
+              <div class="row justify-center">
+                <q-img :src="require('../statics/league-of-legends/icons/champion/' + champion.image.full)" spinner-color="black" class="cursor-pointer" style="height: 48px; max-width: 48px;" />
+              </div>
+              <span class="row justify-center q-pt-xs text-white text-italic text-subtitle2 text-weight-bold">{{ champion.name }}</span>
+            </div>
+          </template>
+        </div>
+      </q-scroll-area>
     </q-page>
   </div>
 </template>
 
 <script>
-import dragontailService from '../services/dragontailService'
+import champions from '../statics/league-of-legends/all-champions.json'
 
 export default {
   data () {
     return {
-      champions: []
+      champions: [],
+      searchChampion: null
     }
   },
 
@@ -31,14 +42,7 @@ export default {
 
   methods: {
     loadChampions () {
-      this.loading = true
-
-      dragontailService.getChampions().then(response => {
-        this.champions = response.data.data
-        this.loading = false
-      }).catch(() => {
-        this.loading = false
-      })
+      this.champions = champions.data
     }
   }
 }
