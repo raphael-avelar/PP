207e3d37dfde1ea001332e4fc8d477fa6764dfec
Raphael Avelar <rafael-avelar@hotmail.com>
2021-06-14 02:41:39 -0300

    Implementando mini menu lateral

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -3,7 +3,7 @@
     <q-header elevated class="bg-black">
       <q-toolbar>
         <q-btn flat @click="leftDrawerOpen = !leftDrawerOpen" round dense icon="menu" />
-        <q-toolbar-title>Projeto P</q-toolbar-title>
+        <q-toolbar-title></q-toolbar-title>
 
         <q-input outlined v-model="nomeInvocador" dense dark filled style="width: 20rem" placeholder="Digite o nome de invocador...">
           <template v-slot:append>
@@ -14,26 +14,34 @@
     </q-header>
 
     <q-drawer
-      v-model="leftDrawerOpen"
+      show-if-above
+      :mini="!leftDrawerOpen"
+      mini-to-overlay
       :width="250"
       :breakpoint="500"
-      overlay
       bordered
       persistent
-      content-class="bg-grey-3"
+      content-class="bg-black"
     >
       <q-scroll-area class="fit">
-        <template v-for="(menuItem, i) in menuList">
-          <router-link :key="i" :to="menuItem.to" custom style="text-decoration: none">
-            <q-item clickable :active="menuItem.label === 'Outbox'" v-ripple>
-              <div class="row no-wrap items-center">
-                <q-icon class="q-pr-md" :name="menuItem.icon" color="primary" size="sm"></q-icon>
-                <span class="text-primary text-weight-bold">{{ menuItem.label }}</span>
-              </div>
-            </q-item>
-            <q-separator :key="'sep' + i" v-if="menuItem.separator" />
-          </router-link>
-        </template>
+        <q-list padding>
+          <template v-for="(menuItem, i) in menuList">
+            <router-link :key="i" :to="menuItem.to" custom style="text-decoration: none">
+              <q-item clickable v-ripple>
+                <q-tooltip v-if="!leftDrawerOpen" anchor="center right" self="center left" :offset="[15, 15]">{{ menuItem.label }}</q-tooltip>
+
+                <q-item-section avatar>
+                  <q-icon :name="menuItem.icon" color="white" size="sm"></q-icon>
+                </q-item-section>
+
+                <q-item-section>
+                  <span class="text-white text-weight-bold">{{ menuItem.label }}</span>
+                </q-item-section>
+              </q-item>
+              <!-- <q-separator :key="'sep' + i" v-if="menuItem.separator" color="white" /> -->
+            </router-link>
+          </template>
+        </q-list>
       </q-scroll-area>
     </q-drawer>
 
